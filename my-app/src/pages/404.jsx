17ec8ac036845aeba57c651c18015ebac01164c5
Sughit <andreisorici2007@gmail.com>
2025-11-14 14:48:35 +0200

    .

--- my-app/src/pages/404.jsx
+++ my-app/src/pages/404.jsx
@@ -1,4 +1,4 @@
-export default function Profile() {
+export default function Page404() {
   return(
     <>
       
--- my-app/src/pages/App.jsx
+++ my-app/src/pages/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import Main from "./Main.jsx";
+import Profile from "./Profile.jsx";
+import Page404 from "./404.jsx";
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind merge 
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/profile" element={<Profile />} /> 
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Router>
   )
 }
